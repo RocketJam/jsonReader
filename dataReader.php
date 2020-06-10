b1316d5d69c6cbac93495cdec349459fdcc2d5f1
IldusKhalitov <rocketjam3@gmail.com>
2020-06-09 21:21:49 -0700

    Delete index.html -- structure and php in datareader.php

---
+++ dataReader.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <title>Data Reader</title>
+    </head>
+    <body>
+        <?php 
+        
+        ?>
+
+    </body>
+</html>
